0a0713ea7d3f5f47ada09dd651510effbd030c5e
Illia Polosukhin <ilblackdragon@gmail.com>
2018-05-30 18:18:05 -0700

    Update website

--- pages/excel.jsx
+++ pages/excel.jsx
@@ -11,13 +11,13 @@ import LandingPage from '../components/LandingPage'
 class Excel extends React.Component {
     render() {
         const steps = [
-            { image: "1.png", text: "Connect your existing online spreadsheet (Google, Excel etc)" },
-            { image: "2.png", text: "Describe connection between spreadsheet and UI" },
-            { image: "3.png", text: "Launch a fully functioning mobile app using our AI technology" },
+            { image: "/1.png", text: "Connect your existing online spreadsheet (Google, Excel etc)" },
+            { image: "/2.png", text: "Describe connection between spreadsheet and UI" },
+            { image: "/3.png", text: "Launch a fully functioning mobile app using our AI technology" },
         ];
         return (
             <DocumentTitle title={ config.siteTitle }>
-                <LandingPage title="Make an app using data from your spreadsheets." subtitle="No code required." steps={steps} main_image="spreadsheet.svg" />
+                <LandingPage title="Make an app using data from your spreadsheets." subtitle="No code required." steps={steps} main_image="/spreadsheet.svg" />
             </DocumentTitle>
         )
     }
